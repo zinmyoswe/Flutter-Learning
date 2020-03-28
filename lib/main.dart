8d61c35d397c9c7b8b55af3ba13d25b03a251935
ZIN MYO <overmidnight12@gmail.com>
2020-03-28 04:19:16 -0700

    my changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,13 @@ void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
       title: Text('my First app'),
+      centerTitle: true,
     ),
-  )
+    body: Center(
+      child: Text('Welcome our Delivery Service'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+    ),
+  ),
 ));
