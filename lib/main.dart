bb5f44a5d6b03eacdaa7bc31d6be56076eda0409
ZIN MYO <overmidnight12@gmail.com>
2020-03-29 00:23:30 -0700

    my changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my First app'),
       centerTitle: true,
+      backgroundColor: Colors.black,
+
     ),
     body: Center(
-      child: Text('Welcome our Delivery Service'),
+      child: Text(
+        'Welcome our Delivery Service',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.black,
+        ),
+      ),
+
     ),
     floatingActionButton: FloatingActionButton(
       child: Text('click'),
+
     ),
   ),
 ));
