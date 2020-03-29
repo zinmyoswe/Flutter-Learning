4a65c81da1ec7eb7b0bf9ebc9b6e4dd5fb57de78
ZIN MYO <overmidnight12@gmail.com>
2020-03-29 00:47:49 -0700

    my changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my First app'),
-      centerTitle: true,
-      backgroundColor: Colors.black,
-
-    ),
-    body: Center(
-      child: Text(
-        'Welcome our Delivery Service',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.black,
-          fontFamily: 'Abel',
-        ),
-      ),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-
-    ),
-  ),
+  home:
 ));
 
-class  extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('my First app'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+
+      ),
+      body: Center(
+        child: Text(
+          'Welcome our Delivery Service',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.black,
+            fontFamily: 'Abel',
+          ),
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+
+      ),
+    );
   }
 }
