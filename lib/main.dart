f7ebeaf80f169734be16f5596cc2c01853a5ee31
ZIN MYO <overmidnight12@gmail.com>
2020-04-02 02:23:48 -0700

    my changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,35 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.black,
 
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.end,
+      body: Row(
         children: <Widget>[
-          Container(
-            color: Colors.lightBlue[200],
-            padding: EdgeInsets.all(30.0),
-            child: Text('Container 1'),
+          Expanded(
+            child: Image.asset('assets/a (1).jpg'),
           ),
-          Container(
-            color: Colors.red[400],
-            padding: EdgeInsets.all(30.0),
-            child: Text('Container 2'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.green[500],
+              child: Text('1'),
+            ),
           ),
-          Container(
-            color: Colors.grey[300],
-            padding: EdgeInsets.all(30.0),
-            child: Text('Container 3'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.red[500],
+              child: Text('2'),
+            ),
           ),
-
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.blue[500],
+              child: Text('3'),
+            ),
+          ),
         ],
       ),
 
